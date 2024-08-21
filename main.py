b6e6c853c7ccb2ab844577d1fc13f802f0d5915e
fisterniklas <fisterniklas@outlook.com>
2024-08-21 22:05:32 +0200

    push

--- main.py
+++ main.py
@@ -17,6 +17,24 @@ def validation(array, target):
 
 print(validation(num_array, target_sum))
 
+
+
 # Problem 3 / Solution 1
 unsorted_array = [12,24,45,9,8,90,3]
 
+index = 0
+next_even = 0
+next_odd = 0
+print(next_odd)
+
+while unsorted_array:
+    if unsorted_array[index] % 2 == 0:
+        next_even = unsorted_array[index]
+    elif unsorted_array[index] % 2 != 0:
+        next_odd = unsorted_array[index]
+    print(unsorted_array[index])
+    index += 1
+    if index == len(unsorted_array):
+        break
+
+print(unsorted_array)

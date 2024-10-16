a869ac2c13d8eaa1112aa16be3d78224d3ddbc97
fisterniklas <fisterniklas@outlook.com>
2024-10-16 20:23:11 +0200

    16.10.24 1946

---
+++ debug.py
@@ -0,0 +1,62 @@
+import heapq
+import sys
+
+putin = sys.stdin.readline
+
+
+def inp():
+    return int(putin())
+
+
+def invert():
+    return map(int, putin().split())
+
+
+def starting_price(main_ar, sec_arr):
+    sub_summ = 0
+    for val_a in sec_arr:
+        sub_summ += val_a[0]
+    for val_b in main_ar:
+        if val_b[0] + val_b[1] >= 0:
+            sub_summ += val_b[0] + val_b[1]
+    return sub_summ
+
+
+if __name__ == "__main__":
+    t = inp()
+    for x in range(t):
+        arr = []
+        k_arr = []
+        n, k = invert()
+        a = list(invert())
+        a = [-x for x in a]
+        b = list(invert())
+        max_value = 0
+        sub_sum = 0
+
+        for i in range(n):
+            arr.append([a[i], b[i]])
+        arr.sort(key=lambda y: y[1], reverse=False)
+
+        for k_range in range(1, k + 1):
+            if arr:
+                if k_range <= len(arr):
+                    heapq.heappush(k_arr, arr[-1])
+                arr.pop()
+
+        if arr:
+            sub_sum = starting_price(arr, k_arr)
+        if sub_sum > max_value:
+            max_value = sub_sum
+        while arr:
+            if k_arr and len(k_arr) == k:
+                if arr[-1][0] + arr[-1][1] >= 0:
+                    sub_sum -= k_arr[0][0] + arr[-1][1]
+                else:
+                    sub_sum += arr[-1][0] -k_arr[0][0]
+            if arr:
+                heapq.heappushpop(k_arr, arr[-1])
+                arr.pop()
+            if sub_sum > max_value:
+                max_value = sub_sum
+        print(max_value)
